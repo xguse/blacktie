e1651b7b2ff02f76fef6b5d7181a0c7dfe17d0eb
Augustine Dunn <wadunn83@gmail.com>
2013-04-19 10:26:31 -0700

    Changed version number to 0.2.0rc1 in code

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.1.1'
+version = '0.2.0rc1'
 
 install_requires = [ 'PyYAML', 'Mako',
     # List your project dependencies here.
