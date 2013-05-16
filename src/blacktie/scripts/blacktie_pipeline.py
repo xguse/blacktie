ed998b22be23f2eb3fe48a38f3b23a8eef84c2c4
Augustine Dunn <wadunn83@gmail.com>
2013-05-16 16:38:54 -0700

    2013-05-16 1639: --prog all skipped cummerbund call.  caused by loop in wrong scope level.  Fixed it.

--- src/blacktie/scripts/blacktie_pipeline.py
+++ src/blacktie/scripts/blacktie_pipeline.py
@@ -230,24 +230,24 @@ def main():
         print "[Note] Skipping cuffdiff step.\n"    
 
 
-        if args.prog in ['cummerbund','all']:
-            
-            # test to make sure R and cummeRbund libs exist
-            from blacktie.scripts import cummerbund
-            cummerbund.import_cummeRbund_library()
-            
-            print "[Note] Starting cummerbund step.\n"
-            for exp_id in yargs.groups:
-    
-                # Prep cummerbund call
-                cummerbund_call = CummerbundCall(yargs,email_info,run_id,run_logs,conditions=exp_id,mode=args.mode)
-                cummerbund_call.execute()
-    
-                # record the cummerbund_call object
-                yargs.call_records[cummerbund_call.call_id] = cummerbund_call
-    
-        else:
-            print "[Note] Skipping cummerbund step.\n"
+    if args.prog in ['cummerbund','all']:
+        
+        # test to make sure R and cummeRbund libs exist
+        from blacktie.scripts import cummerbund
+        cummerbund.import_cummeRbund_library()
+        
+        print "[Note] Starting cummerbund step.\n"
+        for exp_id in yargs.groups:
+
+            # Prep cummerbund call
+            cummerbund_call = CummerbundCall(yargs,email_info,run_id,run_logs,conditions=exp_id,mode=args.mode)
+            cummerbund_call.execute()
+
+            # record the cummerbund_call object
+            yargs.call_records[cummerbund_call.call_id] = cummerbund_call
+
+    else:
+        print "[Note] Skipping cummerbund step.\n"
 
 
 if __name__ == "__main__":
