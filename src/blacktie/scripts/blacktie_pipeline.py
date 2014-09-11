0e8a07ec09d0f691a5f7337ea40c8fed7644bc1c
Augustine Dunn <wadunn83@gmail.com>
2014-09-11 17:01:24 -0400

    blacktie/scripts/blacktie_pipeline.py: - changed --no-email to --email (now it is default NOT to send emails) - made --prog option allow giving multiple valid programs (still leaves it up to user to make sure the needed files exist and are pointed to correctly) - PEP8 - cleaned up imports - now uses outside `bunch` lib

--- src/blacktie/scripts/blacktie_pipeline.py
+++ src/blacktie/scripts/blacktie_pipeline.py
@@ -19,36 +19,36 @@ coordination of data through parts or all of the popular Tophat/Cufflinks
 RNA-seq analysis suite.
 """
 
-import os
+
 import sys
 import argparse
-import base64
-import traceback
 import re
-import time
-import socket
-import shutil
-from collections import defaultdict
 
 import yaml
 
 try:
     import pprocess
 except ImportError:
+    pprocess = False
     pass
 
 import blacktie
 
-from blacktie.utils.misc import Bunch,bunchify
-from blacktie.utils.misc import email_notification
-from blacktie.utils.misc import get_time
+from bunch import bunchify
+
 from blacktie.utils.misc import map_condition_groups
-
-from blacktie.utils.externals import runExternalApp
-from blacktie.utils.externals import mkdirp
-
-from blacktie.utils import errors
 from blacktie.utils.calls import *
+
+
+def start_prog(args_prog, test_vals):
+    """
+    Manage testing whether a program block should be entered based on the contents of args.prog.
+
+    :param args_prog: `list` of program names.
+    :param test_vals: `list` of strings that should trigger returning `True`
+    :return: `bool`
+    """
+    return any((prog in test_vals) for prog in args_prog)
 
 
 def main():
@@ -56,7 +56,8 @@ def main():
     The main loop.  Lets ROCK!
     """
 
-    desc = """This script reads options from a yaml formatted file and organizes the execution of tophat/cufflinks runs for multiple condition sets."""
+    desc = """This script reads options from a yaml formatted file and organizes the execution of tophat/cufflinks
+    runs for multiple condition sets."""
 
     parser = argparse.ArgumentParser(description=desc)
 
@@ -64,16 +65,19 @@ def main():
                         help="""Print version number.""")    
     parser.add_argument('config_file', type=str,
                         help="""Path to a yaml formatted config file containing setup options for the runs.""")
-    parser.add_argument('--prog', type=str, choices=['tophat','cufflinks','cuffmerge','cuffdiff','cummerbund','all'], default='tophat',
+    parser.add_argument('--prog', type=str, nargs='+', choices=['tophat', 'cufflinks', 'cuffmerge', 'cuffdiff',
+                                                                'cummerbund', 'all'], default='tophat',
                         help="""Which program do you want to run? (default: %(default)s)""")
     parser.add_argument('--hide-logs', action='store_true', default=False,
-                        help="""Make your log directories hidden to keep a tidy 'looking' base directory. (default: %(default)s)""")
-    parser.add_argument('--no-email', action='store_true', default=False,
-                        help="""Don't send email notifications. (default: %(default)s)""")
-    parser.add_argument('--mode', type=str, choices=['analyze','dry_run','qsub_script'], default='analyze',
+                        help="""Make your log directories hidden to keep a tidy 'looking' base directory. (default:
+                        %(default)s)""")
+    parser.add_argument('--email', action='store_true', default=False,
+                        help="""Send email notifications. (default: %(default)s)""")
+    parser.add_argument('--mode', type=str, choices=['analyze', 'dry_run', 'qsub_script'], default='analyze',
                         help="""1) 'analyze': run the analysis pipeline. 2) 'dry_run': walk through all steps that
                         would be run and print out the command lines; however, do not send the commands to the
-                        system to be run. 3) 'qsub_script': generate bash scripts suitable to be sent to a compute cluster's
+                        system to be run. 3) 'qsub_script': generate bash scripts suitable to be sent to a compute
+                        cluster's
                         SGE through the qsub command. (default: %(default)s)""")    
 
     if len(sys.argv) == 1:
@@ -82,7 +86,7 @@ def main():
 
     args = parser.parse_args()
 
-    yargs = bunchify(yaml.load(open(args.config_file,'rU')))
+    yargs = bunchify(yaml.load(open(args.config_file, 'rU')))
 
     # set up run_id, log files, and email info
     if yargs.run_options.run_id:
@@ -93,47 +97,43 @@ def main():
 
     base_dir = yargs.run_options.base_dir.rstrip('/')
     if args.hide_logs:
-        run_logs  = '%s/.%s.logs' % (base_dir,run_id)
+        run_logs = '%s/.%s.logs' % (base_dir, run_id)
     else:
-        run_logs  = '%s/%s.logs' % (base_dir,run_id)
-
+        run_logs = '%s/%s.logs' % (base_dir, run_id)
 
     if not args.mode == 'dry_run':
         mkdirp(run_logs)
     else:
         pass
 
-
-    yaml_out = '%s/%s.yaml' % (run_logs,run_id)
-
-
+    yaml_out = '%s/%s.yaml' % (run_logs, run_id)
 
     # copy yaml config file with run_id as name for records
     if not args.mode == 'dry_run':
-        shutil.copyfile(args.config_file,yaml_out)
+        shutil.copyfile(args.config_file, yaml_out)
     else:
         pass
 
-    if not args.no_email:
-        email_info = Bunch({'email_from' : yargs.run_options.email_info.sender,
-                            'email_to' : yargs.run_options.email_info.to,
-                            'email_li' : open(yargs.run_options.email_info.li,'rU').readline().rstrip('\n')})
+    if args.email:
+        email_info = Bunch({'email_from': yargs.run_options.email_info.sender,
+                            'email_to': yargs.run_options.email_info.to,
+                            'email_li': open(yargs.run_options.email_info.li, 'rU').readline().rstrip('\n')})
     else:
-        email_info = Bunch({'email_from' : False,
-                            'email_to' : False,
-                            'email_li' : ''})
+        email_info = Bunch({'email_from': False,
+                            'email_to': False,
+                            'email_li': ''})
 
     yargs.prgbar_regex = re.compile('>.+Processing.+\[.+\].+%\w*$')
     yargs.groups = map_condition_groups(yargs)
     yargs.call_records = {}
 
     # loop through the queued conditions and send reports for tophat 
-    if args.prog in ['tophat','all']:
+    if start_prog(args.prog, ['tophat', 'all']):
         print '[Note] Starting tophat step.\n'
         for condition in yargs.condition_queue:
 
             # Prep Tophat Call
-            tophat_call = TophatCall(yargs,email_info,run_id,run_logs,conditions=condition,mode=args.mode)
+            tophat_call = TophatCall(yargs, email_info, run_id, run_logs, conditions=condition, mode=args.mode)
             tophat_call.execute()
 
             # record the tophat_call object
@@ -141,7 +141,7 @@ def main():
     else:
         print "[Note] Skipping tophat step.\n"
 
-    if args.prog in ['cufflinks','all']:
+    if start_prog(args.prog, ['cufflinks', 'all']):
         # attempt to run more than one cufflinks call in parallel since cufflinks
         # seems to use only one processor no matter the value of -p you give it and
         # doesn't seem to consume massive amounts of memory 
@@ -180,7 +180,8 @@ def main():
             execute = queue.manage(pprocess.MakeParallel(run_cufflinks_call))
             jobs = []
             for condition in yargs.condition_queue:
-                cufflinks_call = CufflinksCall(yargs,email_info,run_id,run_logs,conditions=condition,mode=args.mode)
+                cufflinks_call = CufflinksCall(yargs, email_info, run_id, run_logs, conditions=condition,
+                                               mode=args.mode)
                 cufflinks_call = change_processor_count(cufflinks_call)
                 jobs.append(cufflinks_call)
                 execute(cufflinks_call)
@@ -200,7 +201,8 @@ def main():
             # loop through the queued conditions and send reports for cufflinks    
             for condition in yargs.condition_queue:   
                 # Prep cufflinks_call
-                cufflinks_call = CufflinksCall(yargs,email_info,run_id,run_logs,conditions=condition,mode=args.mode)
+                cufflinks_call = CufflinksCall(yargs, email_info, run_id, run_logs, conditions=condition,
+                                               mode=args.mode)
                 cufflinks_call.execute()
 
                 # record the cufflinks_call object
@@ -208,14 +210,12 @@ def main():
     else:
         print "[Note] Skipping cufflinks step.\n"
 
-
-
-    if args.prog in ['cuffmerge','all']:
+    if start_prog(args.prog, ['cuffmerge', 'all']):
         print "[Note] Starting cuffmerge step.\n"
         for exp_id in yargs.groups:
             
             # Prep cuffmerge call
-            cuffmerge_call = CuffmergeCall(yargs,email_info,run_id,run_logs,conditions=exp_id,mode=args.mode)
+            cuffmerge_call = CuffmergeCall(yargs, email_info, run_id, run_logs, conditions=exp_id, mode=args.mode)
             cuffmerge_call.execute()
 
             # record the cuffmerge_call object
@@ -224,13 +224,12 @@ def main():
     else:
         print "[Note] Skipping cuffmerge step.\n"
 
-
-    if args.prog in ['cuffdiff','all']:
+    if start_prog(args.prog, ['cuffdiff', 'all']):
         print "[Note] Starting cuffdiff step.\n"
         for exp_id in yargs.groups:
 
             # Prep cuffmerge call
-            cuffdiff_call = CuffdiffCall(yargs,email_info,run_id,run_logs,conditions=exp_id,mode=args.mode)
+            cuffdiff_call = CuffdiffCall(yargs, email_info, run_id, run_logs, conditions=exp_id, mode=args.mode)
             cuffdiff_call.execute()
 
             # record the cuffdiff_call object
@@ -239,8 +238,7 @@ def main():
     else:
         print "[Note] Skipping cuffdiff step.\n"    
 
-
-    if args.prog in ['cummerbund','all']:
+    if start_prog(args.prog, ['cummerbund', 'all']):
         
         # test to make sure R and cummeRbund libs exist
         from blacktie.scripts import cummerbund
@@ -250,7 +248,7 @@ def main():
         for exp_id in yargs.groups:
 
             # Prep cummerbund call
-            cummerbund_call = CummerbundCall(yargs,email_info,run_id,run_logs,conditions=exp_id,mode=args.mode)
+            cummerbund_call = CummerbundCall(yargs, email_info, run_id, run_logs, conditions=exp_id, mode=args.mode)
             cummerbund_call.execute()
 
             # record the cummerbund_call object
