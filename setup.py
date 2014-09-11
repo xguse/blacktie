23d0dd01243f19b6e32399080d5cd7793d81d059
Augustine Dunn <wadunn83@gmail.com>
2014-09-11 17:25:37 -0400

    setup.py: - cleaned up imports - PEP8

--- setup.py
+++ setup.py
@@ -1,40 +1,36 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.rst')).read()
 
-
 version = '0.2.1.2'
 
-install_requires = [ 'PyYAML', 'Mako', 'rpy2',
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-]
-
+install_requires = ['PyYAML',
+                    'Mako',
+                    'rpy2',
+                    'sh',
+                    'nose',
+                    'bunch'
+                    ]
 
 setup(name='blacktie',
-    version=version,
-    description="A python wrapper for analysis of RNA-seq data with the popular tophat/cufflinks pipeline.",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='scientific computing RNA-seq tophat cufflinks bowtie CummeRbund',
-    author='Augustine Dunn',
-    author_email='wadunn83@gmail.com',
-    url='https://github.com/xguse/',
-    license='GPL 3',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    entry_points={
-        'console_scripts':
-            ['blacktie=blacktie:main',
-             'blacktie-encode=blacktie.scripts.encode_mail_li_file:main',
-             'blacktie-cummerbund=blacktie.scripts.cummerbund:main']
-    }
-)
+      version=version,
+      description="A python wrapper for analysis of RNA-seq data with the popular tophat/cufflinks pipeline.",
+      long_description=README + '\n\n' + NEWS,
+      classifiers=[],
+      keywords='scientific computing RNA-seq tophat cufflinks bowtie CummeRbund',
+      author='Augustine Dunn',
+      author_email='wadunn83@gmail.com',
+      url='https://github.com/xguse/',
+      license='GPL 3',
+      packages=find_packages('src'),
+      package_dir={'': 'src'}, include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points={
+          'console_scripts':
+              ['blacktie=blacktie:main',
+               'blacktie-encode=blacktie.scripts.encode_mail_li_file:main',
+               'blacktie-cummerbund=blacktie.scripts.cummerbund:main']})
