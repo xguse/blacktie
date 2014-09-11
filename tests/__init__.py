40dab0837ed68f9cc40177f0a663cd87518f0181
Augustine Dunn <wadunn83@gmail.com>
2014-09-11 17:06:10 -0400

    tests/__init__.py: - added file

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py is part of the 'blacktie' package.
+# It was written by Gus Dunn and was created on 9/9/14.
+# 
+# Please see the license info in the root folder of this package.
+
+"""
+=================================================
+__init__.py
+=================================================
+Purpose:
+
+"""
+__author__ = 'Gus Dunn'
+
